b72b80a0b7011caad066b0305c49bc6dabc86eb8
Raghu Betina <raghu@firstdraft.com>
2017-04-28 08:19:22 -0500

    Add all seven routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # CREATE
+  get("/photos/new", { :controller => "pictures", :action => "new_form" })
+  get("/create_photo", { :controller => "pictures", :action => "create_row" })
+
+  # READ
+  get("/photos", { :controller => "pictures", :action => "index" })
+  get("/photos/:the_id", { :controller => "pictures", :action => "show" })
+
+  # UPDATE
+  get("/photos/:la_id/edit", { :controller => "pictures", :action => "edit_form" })
+  get("/update_photo/:le_id", { :controller => "pictures", :action => "update_row" })
+
+  # DELETE
+  get("/delete_photo/:da_id", { :controller => "pictures", :action => "destroy_row" })
 end
